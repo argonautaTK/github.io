a70f882b0219232f2e00a32ff237f526fddb8896
argonautaTK <logosnauta69@gmail>
2026-02-10 16:35:00 +0100

    Publicar Draft web

---
+++ assets/main.js
@@ -0,0 +1,86 @@
+//   Cambio de tema claro - oscuro
+// ======================================
+const toggle = document.getElementById("themeToggle");
+const html = document.documentElement;
+const body = document.body;
+
+const setTheme = (theme) => {
+  html.setAttribute("data-theme", theme);
+  html.style.colorScheme = theme;
+  toggle.textContent = theme === "dark" ? "☀️" : '🌗';
+  localStorage.setItem('theme', theme); //#
+};
+
+const savedTheme = localStorage.getItem('theme');// || "light"; //#º
+setTheme(savedTheme);
+
+toggle.addEventListener("click", () => {
+  const current = html.getAttribute("data-theme");
+  setTheme(current === "dark" ? "light" : "dark");
+  //html.style.colorScheme = theme;
+});
+
+//  Gestion de boton flotante 
+// ======================================
+const btnTop = document.getElementById('btn-top');
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 200) {
+    btnTop.classList.add('show');
+  } else {
+    btnTop.classList.remove('show');
+  }
+});
+btnTop.addEventListener('click', () => {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth'
+  });
+});
+
+// Fade-in tarjetas
+// ======================================
+const cards = document.querySelectorAll('.gallery-card');
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('animate-fadeInUp');
+      entry.target.style.opacity = 1;
+      entry.target.style.transform = 'translateY(0)';
+      observer.unobserve(entry.target);
+    }
+  });
+}, { threshold: 0.15 });
+cards.forEach(card => observer.observe(card));
+
+// Lightbox
+// ======================================
+const lightbox = document.getElementById('lightbox');
+const lightboxImg = document.getElementById('lightboxImg');
+const closeBtn = document.getElementById('closeBtn');
+
+document.querySelectorAll('.gallery-card').forEach(card => {
+  card.addEventListener('click', () => {
+    lightboxImg.src = card.dataset.src;
+    lightboxImg.alt = card.dataset.alt;
+    lightbox.classList.add('show');
+    document.body.style.overflow = 'hidden';
+  });
+});
+
+document.querySelectorAll('.gallery-card').forEach(card => {
+    card.addEventListener('click', () => {
+        lightboxImg.src = card.dataset.src;
+        lightboxImg.alt = card.dataset.alt;
+        lightbox.classList.add('show');
+        document.body.style.overflow = 'hidden';
+    });
+});
+
+function closeLightbox() {
+    lightbox.classList.remove('show');
+    document.body.style.overflow = '';
+}
+
+closeBtn.addEventListener('click', closeLightbox);
+lightbox.addEventListener('click', e => { if(e.target === lightbox) closeLightbox(); });
+document.addEventListener('keydown', e => { if(e.key === 'Escape') closeLightbox(); });
